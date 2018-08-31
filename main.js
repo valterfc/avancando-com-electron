0f7faf43854a6e89fd70408c841edd5d29488df8
ValterFC <valter@accellog.com>
2018-08-31 19:40:23 -0300

    reduzindo o número de mensagens no update e adicionando descrição

--- main.js
+++ main.js
@@ -140,7 +140,6 @@ function sendStatusToWindow(text){
 }
 
 function checkForUpdate(){
-
     //eventos do updater
     autoUpdater.on('checking-for-update', () => {
         sendStatusToWindow('Checking for update...');
@@ -150,24 +149,24 @@ function checkForUpdate(){
         sendStatusToWindow('Update available.');
     });
 
-    autoUpdater.on('update-not-available', (info) => {
-        sendStatusToWindow('Update not available.');
-    });
+    //autoUpdater.on('update-not-available', (info) => {
+    //    sendStatusToWindow('Update not available.');
+    //});
 
     autoUpdater.on('error', (err) => {
         sendStatusToWindow('Error in auto-updater. ' + err);
     });
 
-    autoUpdater.on('download-progress', (progressObj) => {
-        let log_message = "Download speed: " + progressObj.bytesPerSecond;
-        log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
-        log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ' )';
-        sendStatusToWindow(log_message);
-    });
+    //autoUpdater.on('download-progress', (progressObj) => {
+    //    let log_message = "Download speed: " + progressObj.bytesPerSecond;
+    //    log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
+    //    log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ' )';
+    //    sendStatusToWindow(log_message);
+    //});
 
-    autoUpdater.on('update-downloaded', (info) => {
-        sendStatusToWindow('Update downloaded');
-    });
+    //autoUpdater.on('update-downloaded', (info) => {
+    //    sendStatusToWindow('Update downloaded');
+    //});
 
     autoUpdater.checkForUpdatesAndNotify(); //verifica as atualizações
 }
